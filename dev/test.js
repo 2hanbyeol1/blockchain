0eccba15406d9f256abc908bfe3405e8c94739c5
2hanbyeol1 <2hanbyeol1@naver.com>
2023-10-01 07:40:54 +0900

    [TEST] createNewBlock

--- dev/test.js
+++ dev/test.js
@@ -1,3 +1,6 @@
 const Blockchain = require("./blockchain");
 const bitcoin = new Blockchain();
+bitcoin.createNewBlock(2389, "A1S2DF5DF6SD5", "59SFS8AS4DJK");
+bitcoin.createNewBlock(2389, "ODI798DSF15SD", "SFSDF8DF4456");
+bitcoin.createNewBlock(2389, "13SDFSDF5JKRQ", "CCM548SF22FD");
 console.log(bitcoin);
