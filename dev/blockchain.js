d035d15ed487b6190d5986b4bbb6b507222bfc49
2hanbyeol1 <2hanbyeol1@naver.com>
2023-10-01 08:25:55 +0900

    [TEST] hashBlock

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -6,7 +6,6 @@ function Blockchain() {
 /**
  * @param nonce 자격증명에서 온 숫자 값
  * proofOfWork 메소드를 통해 적법하게 새로운 블록을 만들었다는 증거
- * @param hash newTransactions를 해싱 함수에 전달했을 때 생성된 문자열
  */
 Blockchain.prototype.createNewBlock = function (
   nonce,
--- dev/test.js
+++ dev/test.js
@@ -1,10 +1,23 @@
 const Blockchain = require("./blockchain");
 const bitcoin = new Blockchain();
-bitcoin.createNewBlock(2389, "A1S2DF5DF6SD5", "59SFS8AS4DJK");
-bitcoin.createNewTransaction(100, "ALEX98DSF15SD", "JENNF8DF4456");
-bitcoin.createNewBlock(54487, "AKMCDF5DF6SD5", "WPLSS8AS4DJK");
-bitcoin.createNewTransaction(50, "ALEX98DSF15SD", "JENNF8DF4456");
-bitcoin.createNewTransaction(200, "ALEX98DSF15SD", "JENNF8DF4456");
-bitcoin.createNewTransaction(300, "ALEX98DSF15SD", "JENNF8DF4456");
-bitcoin.createNewBlock(54487, "SDF45SDFSD12M", "SDI45FS9456FDS");
-console.log(bitcoin.chain[2]);
+
+const previousBlockHash = "87765DA6CCF0668238C1D27C35692E11";
+const currentBlockData = [
+  {
+    amount: 10,
+    sender: "B4CEE9C0E5CD793",
+    recipient: "ASDFDFSDFI12SDSD",
+  },
+  {
+    amount: 20,
+    sender: "ABSDF897985CD793",
+    recipient: "3ADFSDFI12SDSD",
+  },
+  {
+    amount: 30,
+    sender: "C4SDF354C0E5CD793",
+    recipient: "BSDF98795FI1SD",
+  },
+];
+const nonce = 100;
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
