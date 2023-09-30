ae3a19c0b5b90262ce9f530b7020b8b1c3c831a0
2hanbyeol1 <2hanbyeol1@naver.com>
2023-10-01 07:47:18 +0900

    [FEAT] createNewTransaction

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,6 +1,6 @@
 function Blockchain() {
   this.chain = [];
-  this.newTransactions = [];
+  this.pendingTransactions = [];
 }
 
 /**
@@ -16,19 +16,40 @@ Blockchain.prototype.createNewBlock = function (
   const newBlock = {
     index: this.chain.length + 1,
     timestamp: Date.now(),
-    transactions: this.newTransactions,
+    transactions: this.pendingTransactions,
     nonce: nonce,
     hash: hash,
     previousBlockHash,
     previousBlockHash,
   };
-  this.newTransactions = [];
+  this.pendingTransactions = [];
   this.chain.push(newBlock);
   return newBlock;
 };
 
 Blockchain.prototype.getLastBlock = function () {
   return this.chain[this.chain.length - 1];
+};
+
+/**
+ *
+ * @param amount 금액
+ * @param sender 발송인 주소
+ * @param recipient 수신자 주소
+ * @returns 새로운 트랜잭션이 추가될 블록의 num
+ */
+Blockchain.prototype.createNewTransaction = function (
+  amount,
+  sender,
+  recipient
+) {
+  const newTransaction = {
+    amount: amount,
+    sender: sender,
+    recipient: recipient,
+  };
+  this.pendingTransactions.push(newTransaction);
+  return this.getLastBlock()["index"] + 1;
 };
 
 module.exports = Blockchain;
