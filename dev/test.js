341294485d29a4f787284519747fe6494e10c07b
2hanbyeol1 <2hanbyeol1@naver.com>
2023-10-01 07:49:25 +0900

    [TEST] createNewTransaction

--- dev/test.js
+++ dev/test.js
@@ -1,6 +1,5 @@
 const Blockchain = require("./blockchain");
 const bitcoin = new Blockchain();
 bitcoin.createNewBlock(2389, "A1S2DF5DF6SD5", "59SFS8AS4DJK");
-bitcoin.createNewBlock(2389, "ODI798DSF15SD", "SFSDF8DF4456");
-bitcoin.createNewBlock(2389, "13SDFSDF5JKRQ", "CCM548SF22FD");
+bitcoin.createNewTransaction(100, "ALEX98DSF15SD", "JENNF8DF4456");
 console.log(bitcoin);
