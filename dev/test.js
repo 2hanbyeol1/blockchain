33d1e4bb6a52a15aa42a0870ade9229d76bbe9f6
2hanbyeol1 <2hanbyeol1@naver.com>
2023-10-01 09:28:29 +0900

    [TEST] 제네시스 블록

--- dev/test.js
+++ dev/test.js
@@ -1,25 +1,4 @@
 const Blockchain = require("./blockchain");
 const bitcoin = new Blockchain();
 
-const previousBlockHash = "87765DA6CCF0668238C1D27C35692E11";
-const currentBlockData = [
-  {
-    amount: 10,
-    sender: "B4CEE9C0E5CD793",
-    recipient: "ASDFDFSDFI12SDSD",
-  },
-  {
-    amount: 20,
-    sender: "ABSDF897985CD793",
-    recipient: "3ADFSDFI12SDSD",
-  },
-  {
-    amount: 30,
-    sender: "C4SDF354C0E5CD793",
-    recipient: "BSDF98795FI1SD",
-  },
-];
-
-const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
-console.log(nonce);
-console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
+console.log(bitcoin);
