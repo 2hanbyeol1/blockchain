51599868e24c359bd7d0b1af34d3d8b3b44e22d7
2hanbyeol1 <2hanbyeol1@naver.com>
2023-10-24 21:15:21 +0900

    [FEAT] /mine 엔드포인트 구축

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const { v4: uuidv4 } = require("uuid");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,7 +25,32 @@ app.post("/transaction", function (req, res) {
 });
 
 // 새 블록을 채굴한다
-app.get("/mine", function (req, res) {});
+app.get("/mine", function (req, res) {
+  const lastBlock = bitcoin.getLastBlock();
+  const previousBlockHash = lastBlock["hash"];
+
+  const currentBlockData = {
+    transactions: bitcoin.pendingTransactions,
+    index: lastBlock["index"] + 1,
+  };
+  const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
+
+  const blockHash = bitcoin.hashBlock(
+    previousBlockHash,
+    currentBlockData,
+    nonce
+  );
+
+  const nodeAddress = uuidv4().split("-").join("");
+  bitcoin.createNewTransaction(12.5, "00", nodeAddress); // 채굴 보상 - 송신자 주소 "00"
+
+  const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, blockHash);
+
+  res.json({
+    note: "New block mined successfully",
+    block: newBlock,
+  });
+});
 
 app.listen(3000, function () {
   console.log("Listening on port 3000..."); // 포트가 잘 동작해야 실행됨
